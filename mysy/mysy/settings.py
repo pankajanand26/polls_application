1f55f29901e2506e087455a6b6c1f019232c88d3
Pankaj Anand <pankaj.anand.26@gmail.com>
2015-01-31 15:13:08 +0000

    Project Creation

--- mysy/mysy/settings.py
+++ mysy/mysy/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -58,8 +59,10 @@ WSGI_APPLICATION = 'mysy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysy',
+        'USER': 'root',
+	'HOST': '0.0.0.0',
     }
 }
 
