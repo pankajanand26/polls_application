f91ed1089ab694446bf152ba779deeb1d0567dd8
Pankaj Anand <pankaj.anand.26@gmail.com>
2015-02-01 11:28:10 +0000

    Poll app with admin

--- mysy/mysy/settings.py
+++ mysy/mysy/settings.py
@@ -98,7 +98,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_ROOT = os.path.join(, '..', '..', 'static')
+STATIC_ROOT = os.path.join( '..', '..', 'static')
 
 STATIC_URL = '/static/'
 
