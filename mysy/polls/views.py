5d416725f553562f2f220c505262870d5cb941c8
Pankaj Anand <pankaj.anand.26@gmail.com>
2015-02-02 20:38:04 +0000

    Databas Connection setup

--- mysy/polls/views.py
+++ mysy/polls/views.py
@@ -2,11 +2,17 @@
 from django.http import HttpResponse
 # Create your views here.
 from polls.models import Question
+from django.template import RequestContext, loader
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([p.question_text for p in latest_question_list])
-    return HttpResponse(output)
+#    output = ', '.join([p.question_text for p in latest_question_list])
+#    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {
+        'latest_question_list': latest_question_list,
+    })
+    return HttpResponse(template.render(context))
 
 def detail(request, question_id):
     response = "You're looking at the results of question %s."
