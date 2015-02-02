36ad345a8034a177585972917403b045647893d1
Pankaj Anand <pankaj.anand.26@gmail.com>
2015-02-02 21:04:01 +0000

    View Detailing

--- mysy/polls/views.py
+++ mysy/polls/views.py
@@ -1,6 +1,7 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import Http404
+from django.shortcuts import render
 # Create your views here.
 from polls.models import Question
 from django.template import RequestContext, loader
