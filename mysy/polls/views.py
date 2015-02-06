c4a478f86874a6e88ea41d1acac54756c83967d9
Pankaj Anand <pankaj.anand.26@gmail.com>
2015-02-06 21:38:40 +0000

    Fully functional app

--- mysy/polls/views.py
+++ mysy/polls/views.py
@@ -29,9 +29,26 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
+    question = get_object_or_404(Question, pk=question_id) 
+    return render(request, 'polls/results.html', {'question': question})    
+
+response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    p = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', { 
+            'question': p, 
+            'error_message': "You didn't select a choice.", 
+        }) 
+    else: 
+        selected_choice.votes += 1 
+        selected_choice.save() 
+        # Always return an HttpResponseRedirect after successfully dealing 
+        # with POST data. This prevents data from being posted twice if a 
+        # user hits the Back button. 
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
 
