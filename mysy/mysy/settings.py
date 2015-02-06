9134cde71dcc5407e5313139f3155b1dc29ee923
Pankaj Anand <pankaj.anand.26@gmail.com>
2015-02-06 20:26:11 +0000

    trying to set the email chnage

--- mysy/mysy/settings.py
+++ mysy/mysy/settings.py
@@ -136,3 +136,4 @@ EMAIL_HOST_USER = 'pankaj.anand.26@gmail.com'
 EMAIL_PORT      = 587
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 DEFAULT_FROM_EMAIL  = 'admin@mysy.com'
+EMAIL_USE_TLS = True
