826afeda4212dbde367d11fbdadee5f07cba7311
Pankaj Anand <pankaj.anand.26@gmail.com>
2015-02-06 20:38:02 +0000

    trying to set the email chnage

--- mysy/polls/views.py
+++ mysy/polls/views.py
@@ -15,7 +15,7 @@ def index(request):
     context = RequestContext(request, {
         'latest_question_list': latest_question_list,
     })
-    send_mail('Subject here', 'Here is the message.', 'pankaj.anand.26@gmail.com',     ['pankaj.anand.26@gmail.com','pankaj_anand_26@yahoo.co.in'], fail_silently=False)
+    send_mail('iSmile Please', 'Here is the message.', 'pankaj.anand.26@gmail.com',     ['pankaj.anand.26@gmail.com','pankaj_anand_26@yahoo.co.in'], fail_silently=False)
     return HttpResponse(template.render(context))
 
 def detail(request, question_id):
