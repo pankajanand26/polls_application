853c5e3443763c64c60a527a012724966cec7340
Pankaj Anand <pankaj.anand.26@gmail.com>
2015-02-07 19:38:21 +0000

    static files prctise

--- mysy/mysy/settings.py
+++ mysy/mysy/settings.py
@@ -117,7 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR,  'static')
+#STATIC_ROOT = os.path.join(BASE_DIR,  'static')
+
+STATIC_ROOT=''
 
 STATIC_URL ='/static/'
 
