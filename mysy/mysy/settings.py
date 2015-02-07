8d6e30feacff4d02ed7e4eee8864690b5f688b30
Pankaj Anand <pankaj.anand.26@gmail.com>
2015-02-07 18:41:41 +0000

    static files prctise

--- mysy/mysy/settings.py
+++ mysy/mysy/settings.py
@@ -117,11 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, '..', '..', 'static', 'static/admin')
+STATIC_ROOT = os.path.join(BASE_DIR,  'static')
 
 STATIC_URL = '/static/'
 
-ADMIN_MEDIA_PREFIX = '/static/admin/'
+#ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 STATICFILES_FINDERS = (
      'django.contrib.staticfiles.finders.FileSystemFinder',
