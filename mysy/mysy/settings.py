f0b57e0d24957c2893b02c801db9045eacedb173
Pankaj Anand <pankaj.anand.26@gmail.com>
2015-02-07 19:55:44 +0000

    static files prctise

--- mysy/mysy/settings.py
+++ mysy/mysy/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 #STATIC_ROOT = os.path.join(BASE_DIR,  'static')
 
-STATIC_ROOT=os.path.join(BASE_DIR,  'static'
+STATIC_ROOT=os.path.join(BASE_DIR,  'static')
 
 STATIC_URL ='/static/'
 
