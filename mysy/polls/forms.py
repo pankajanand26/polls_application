c352ac875c9cd30b5f519a867db7e33e37666582
Pankaj Anand <pankaj.anand.26@gmail.com>
2015-02-25 11:45:19 +0000

    Bootstrap

--- mysy/polls/forms.py
+++ mysy/polls/forms.py
@@ -9,5 +9,5 @@ class UserRegForm(forms.Form):
     password2 = forms.CharField(widget=forms.PasswordInput)
 
 class LoginForm(forms.Form):
-    username = forms.CharField(required=True)
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(required=True,widget=forms.TextInput(attrs={'placeholder': 'username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'password'}))
